181536da489b1ad0f582c9fd997cd596d8a4658b
mac Fumiya <fumipumisecky@gmail.com>
2016-03-02 19:18:08 +0900

    add ominiauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:twitter]
+
+  class << self
+    def from_omniauth(auth)
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.username = auth.info.nickname
+        nser.email = auth.info.email
 end
