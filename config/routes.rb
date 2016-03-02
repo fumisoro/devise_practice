8a420a65769d52ac4561049a95d19cfc21327ef3
mac Fumiya <fumipumisecky@gmail.com>
2016-03-02 19:20:34 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbakcs: "omniauth_callbacks" }
   devise_scope :user do
     root "devise/sessions#new"
   end
